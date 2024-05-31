618843bad64fb47d6f8ce32e5c545b0439bffc31
Administrator <wltngus200@gmail.com>
2024-05-31 21:18:46 +0900

    0531 정규표현식 다시 + 폴더지우기를 다시 클래스 틀렸음 ㅋㅋ

--- src/main/java/com/green/fristproject1/user/UserService.java
+++ src/main/java/com/green/fristproject1/user/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 import static com.green.fristproject1.user.common.IDCheck.*;
 
 @Service
